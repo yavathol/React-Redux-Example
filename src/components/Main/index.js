367446b3a117cc1bd0b24004005c4654c07e399a
ssitko <ssitko@pgs-soft.com>
2018-02-13 07:11:36 +0100

    Fixed Report & Reports component names typo

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -11,7 +11,7 @@ class Main extends Component {
         return (
             <Row>
                 <Add />
-                <Raports raportsList={[]} />
+                <Raports />
             </Row>
         )
     }
